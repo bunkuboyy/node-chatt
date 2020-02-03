9cacb914b864f58c676560fd8dbca38241414885
Bunkuboy <bunkuboy@gmail.com>
2020-02-04 00:30:06 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+
+const publicpath = path.join(__dirname ,'../public');
+console.log(publicpath);
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static(publicpath));
+
+
+app.listen(port,() =>{
+  console.log(`Starting on port ${port}`);
+})
