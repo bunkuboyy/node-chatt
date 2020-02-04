cccee915b9e5639b414d303a8e8bb755dcef08a6
Bunkuboy <bunkuboy@gmail.com>
2020-02-05 02:43:53 +0530

    greet others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ var io = SOCKETIO(server);
 io.on('connection', (socket) =>{
   console.log('new user connected');
 
+socket.emit('newMessage',{
+  from: 'Admin',
+  text :'Welcome to the chat app',
+  createdAt : new Date().getTime()
+});
+
+socket.broadcast.emit('newMessage',{
+  from : 'Admin',
+  text : 'New user joined',
+  createdAt : new Date().getTime()
+});
 
   socket.on('createMessage', (message)=>{
     console.log('Message created',message);
@@ -23,6 +34,7 @@ io.on('connection', (socket) =>{
       text: message.text,
       createdAt : new Date().getTime()
     })
+
   })
 
   socket.on('disconnect',() =>{
