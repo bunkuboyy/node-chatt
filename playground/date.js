6a295651b8462eb24e0d975c36cdd52b60d63d3e
Bunkuboy <bunkuboy@gmail.com>
2020-02-11 00:05:24 +0530

    moment

---
+++ playground/date.js
@@ -0,0 +1,12 @@
+const moment = require('moment');
+
+// var date = new Date().getTime();
+// console.log(date);
+//
+// var date = moment();
+// date.add(5,'days');
+// console.log(date.format('do MMM YYYY'));
+
+
+var date = moment();
+console.log(date.format('h:mma'));
